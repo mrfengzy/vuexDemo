944661b87e53e4fb70ac3ec2d8926542943b1b82
zy <1002115829@qq.com>
2020-02-19 21:50:57 +0800

    初始化计数器基本结构

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,22 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div>
+    <my-add></my-add>
+    <p>---</p>
+    <my-reduce></my-reduce>
   </div>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import Add from './components/Add.vue'
+import Reduce from './components/Reduce.vue'
+export default {
+  data() {
+    return {}
+  },
+  components: {
+    'my-add': Add,
+    'my-reduce': Reduce
+  }
 }
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+</script>
+<style scoped>
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,22 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    count:0
   },
   mutations: {
+    add(){
+      this.count += 1
+    },
+    reduce() {
+      this.count -= 1
+    }
   },
   actions: {
   },
   modules: {
+    
+  },
+  getters:{
+    
   }
 }) 
